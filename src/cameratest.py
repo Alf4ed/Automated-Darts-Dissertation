4f28e7933f8ca5f2afce90349a9a9b731b6dfcf9
Alfred Carpenter <alfredcarpenter314@gmail.com>
2023-10-24 19:32:28 +0100

    Plot line through largest contour

--- src/cameratest.py
+++ src/cameratest.py
@@ -5,6 +5,12 @@ video_capture_1 = cv2.VideoCapture(0)
 video_capture_2 = cv2.VideoCapture(1)
 video_capture_3 = cv2.VideoCapture(3)
 
+ret1, frame1 = video_capture_1.read()
+height, width, layers = frame1.shape
+resizeHeight = int(height/1.5)
+resizeWidth = int(width/1.5)
+before = cv2.resize(frame1, (resizeWidth, resizeHeight))
+
 while True:
     # Capture frame-by-frame
     ret1, frame1 = video_capture_1.read()
@@ -19,13 +25,55 @@ while True:
     resFrame2 = cv2.resize(frame2, (resizeWidth, resizeHeight))
     resFrame3 = cv2.resize(frame3, (resizeWidth, resizeHeight))
 
-    cv2.line(resFrame1, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
-    cv2.line(resFrame2, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
-    cv2.line(resFrame3, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
+    after = resFrame1
 
-    horizontal = np.hstack([resFrame1, resFrame2, resFrame3])
 
-    cv2.imshow('Dartboard', horizontal)
+
+
+    diff = cv2.subtract(before, after)
+
+    grayscale = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    grayscale = 255 - grayscale
+
+    ret,thresh = cv2.threshold(grayscale, 40, 255, cv2.THRESH_BINARY)
+
+    blur = cv2.GaussianBlur(grayscale,(5,5),0)
+    grayscale = cv2.medianBlur(grayscale, 5)
+
+    adaptiveGaussThresh = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 1)
+
+    adaptiveGaussThresh = 255 - adaptiveGaussThresh
+
+    kernelOpen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    kernelClose = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,17))
+    morphological = cv2.morphologyEx(adaptiveGaussThresh, cv2.MORPH_OPEN, kernelOpen)
+    morphological = cv2.morphologyEx(morphological, cv2.MORPH_CLOSE, kernelClose)
+
+    # contours, hierarchy = cv2.findContours(morphological, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cnt = max(contours, key = cv2.contourArea)
+    # lowest = tuple(cnt[cnt[:, :, 1].argmax()][0])
+    # after = cv2.circle(after, lowest, radius=2, color=(0, 255, 0), thickness=-1)
+
+    cv2.imshow('Contour', morphological)
+
+    before = resFrame1
+
+
+
+
+
+
+
+
+
+
+    # cv2.line(resFrame1, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
+    # cv2.line(resFrame2, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
+    # cv2.line(resFrame3, (int(resizeWidth/2), 0), (int(resizeWidth/2), resizeHeight), (255,0,255), 2)
+
+    # horizontal = np.hstack([resFrame1, resFrame2, resFrame3])
+
+    # cv2.imshow('Dartboard', horizontal)
 
     k = cv2.waitKey(500)
     if k == ord('q'):
--- src/detection.py
+++ src/detection.py
@@ -1,30 +1,141 @@
 import cv2, numpy as np
+from matplotlib import pyplot as plt
+import os, re
+from skimage import data, img_as_ubyte
+from skimage.filters import threshold_isodata, threshold_li, threshold_minimum, threshold_yen, threshold_triangle, threshold_otsu
 
-before = cv2.imread('1710-190431frame1.jpg')
-after = cv2.imread('1710-190447frame1.jpg')
 
-before = cv2.imread('1710-190431frame2.jpg')
-after = cv2.imread('1710-190447frame2.jpg')
+rootdir = 'C:/Users/alfre/Documents/cs310/images'
 
-before = cv2.imread('1710-190431frame3.jpg')
-after = cv2.imread('1710-190447frame3.jpg')
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if re.search("Before", file) is not None:
 
-diff = cv2.subtract(before, after)
+            # Load the before and after images
+            before = cv2.imread(os.path.join(subdir, file))
+            after = cv2.imread(os.path.join(subdir, re.sub('Before', 'After', file)))
 
-grayscale = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+            diff = cv2.subtract(before, after)
+            grayscale = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+            blurred = cv2.medianBlur(grayscale, 5)
 
-ret,thresh = cv2.threshold(grayscale, 20, 255, cv2.THRESH_BINARY)
+            # Normalise
+            norm_img1 = cv2.normalize(blurred, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            norm_img1 = (255*norm_img1).astype(np.uint8)
+            inverted = 255 - norm_img1
 
-kernelOpen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-kernelClose = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-morphological = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernelOpen)
-morphological = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernelClose)
+            # Thresholding operations
+            ret, thresh = cv2.threshold(norm_img1, 25, 255, cv2.THRESH_BINARY)
+            ret3,otsuCV = cv2.threshold(norm_img1, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            adaptiveMeanThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 5)
+            adaptiveGaussThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
 
-# cv2.imshow('Before', before)
-# cv2.imshow('After', after)
-# cv2.imshow('Difference', diff)
-# cv2.imshow('Grayscale', grayscale)
-# cv2.imshow('Thresholding', thresh)
-cv2.imshow('Morphological', morphological)
+            # Morphological operations to remove noise
+            # kernelOpen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,3))
+            # kernelClose = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,9))
+            # morphological = cv2.morphologyEx(adaptiveGaussThresh, cv2.MORPH_OPEN, kernelOpen)
+            # closed = cv2.morphologyEx(morphological, cv2.MORPH_CLOSE, kernelClose)
 
-k = cv2.waitKey(0)
+            triangle = threshold_triangle(norm_img1)
+            yen = threshold_yen(norm_img1)
+            li = threshold_li(norm_img1)
+            min = threshold_minimum(norm_img1)
+            otsu = threshold_otsu(norm_img1)
+            isodata = threshold_isodata(norm_img1)
+            ret, triangle = cv2.threshold(norm_img1, triangle, 255, cv2.THRESH_BINARY)
+            ret, yen = cv2.threshold(norm_img1, yen, 255, cv2.THRESH_BINARY)
+            ret, li = cv2.threshold(norm_img1, li, 255, cv2.THRESH_BINARY)
+            ret, min = cv2.threshold(norm_img1, min, 255, cv2.THRESH_BINARY)
+            ret, otsu = cv2.threshold(norm_img1, otsu, 255, cv2.THRESH_BINARY)
+            ret, isodata = cv2.threshold(norm_img1, isodata, 255, cv2.THRESH_BINARY)
+
+            img = thresh
+
+            all = np.zeros(img.shape)
+            all = cv2.normalize(255 - adaptiveMeanThresh, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(yen, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(otsuCV, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(min, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(otsu, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(isodata, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(thresh, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(255 - adaptiveGaussThresh, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(triangle, all, 0, 25, cv2.NORM_MINMAX)\
+                    + cv2.normalize(li, all, 0, 25, cv2.NORM_MINMAX)
+
+            ret, all = cv2.threshold(all, 76, 255, cv2.THRESH_BINARY)
+
+            # Find the lowest y point of the max size contour
+            contours, hierarchy = cv2.findContours(all, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cnt = max(contours, key = cv2.contourArea)
+            lowest = tuple(cnt[cnt[:, :, 1].argmax()][0])
+            after = cv2.circle(after, lowest, radius=5, color=(0, 0, 255), thickness=-1)
+
+            rows,cols = all.shape[:2]
+            [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+            lefty = int((-x*vy/vx) + y)
+            righty = int(((cols-x)*vy/vx)+y)
+            cv2.line(after,(cols-1,righty),(0,lefty),(0,255,0),2)
+
+            # Plot the results
+            ax1 = plt.subplot(3, 5, 1)
+            ax1.imshow(cv2.cvtColor(after, cv2.COLOR_BGR2RGB))
+            ax1.title.set_text('After')
+
+            ax2 = plt.subplot(3, 5, 2, sharex=ax1, sharey=ax1)
+            ax2.imshow(cv2.cvtColor(diff, cv2.COLOR_BGR2RGB))
+            ax2.title.set_text('Difference')
+
+            ax3 = plt.subplot(3, 5, 3, sharex=ax1, sharey=ax1)
+            ax3.imshow(cv2.cvtColor(grayscale, cv2.COLOR_BGR2RGB))
+            ax3.title.set_text('Grayscale')
+
+            ax4 = plt.subplot(3, 5, 4, sharex=ax1, sharey=ax1)
+            ax4.imshow(cv2.cvtColor(blurred, cv2.COLOR_BGR2RGB))
+            ax4.title.set_text('Blurred')
+
+            ax5 = plt.subplot(3, 5, 5, sharex=ax1, sharey=ax1)
+            ax5.imshow(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
+            ax5.title.set_text('Global')
+
+            ax6 = plt.subplot(3, 5, 6, sharex=ax1, sharey=ax1)
+            ax6.imshow(cv2.cvtColor(otsuCV, cv2.COLOR_BGR2RGB))
+            ax6.title.set_text('OtsuCV')
+
+            ax7 = plt.subplot(3, 5, 7, sharex=ax1, sharey=ax1)
+            ax7.imshow(cv2.cvtColor(255 - adaptiveMeanThresh, cv2.COLOR_BGR2RGB))
+            ax7.title.set_text('Mean')
+
+            ax8 = plt.subplot(3, 5, 8, sharex=ax1, sharey=ax1)
+            ax8.imshow(cv2.cvtColor(255 - adaptiveGaussThresh, cv2.COLOR_BGR2RGB))
+            ax8.title.set_text('Gaussian')
+
+            ax10 = plt.subplot(3, 5, 9, sharex=ax1, sharey=ax1)
+            ax10.imshow(cv2.cvtColor(norm_img1, cv2.COLOR_BGR2RGB))
+            ax10.title.set_text('Normalised')
+
+            ax10 = plt.subplot(3, 5, 10, sharex=ax1, sharey=ax1)
+            ax10.imshow(cv2.cvtColor(triangle, cv2.COLOR_BGR2RGB))
+            ax10.title.set_text('Triangle')
+
+            ax10 = plt.subplot(3, 5, 11, sharex=ax1, sharey=ax1)
+            ax10.imshow(cv2.cvtColor(yen, cv2.COLOR_BGR2RGB))
+            ax10.title.set_text('Yen')
+
+            ax11 = plt.subplot(3, 5, 12, sharex=ax1, sharey=ax1)
+            ax11.imshow(cv2.cvtColor(li, cv2.COLOR_BGR2RGB))
+            ax11.title.set_text('Li')
+
+            ax10 = plt.subplot(3, 5, 13, sharex=ax1, sharey=ax1)
+            ax10.imshow(cv2.cvtColor(min, cv2.COLOR_BGR2RGB))
+            ax10.title.set_text('Min')
+
+            ax10 = plt.subplot(3, 5, 14, sharex=ax1, sharey=ax1)
+            ax10.imshow(cv2.cvtColor(all, cv2.COLOR_BGR2RGB))
+            ax10.title.set_text('All')
+
+            ax11 = plt.subplot(3, 5, 15, sharex=ax1, sharey=ax1)
+            ax11.imshow(cv2.cvtColor(isodata, cv2.COLOR_BGR2RGB))
+            ax11.title.set_text('Isodata')
+
+            plt.show()
