be0c570b3aed525b336d18a140c9bf17261c68a0
Alfred Carpenter <alfredcarpenter314@gmail.com>
2023-11-06 12:48:43 +0000

    Polar coordinate scoring, sample images

--- src/detection.py
+++ src/detection.py
@@ -3,152 +3,186 @@ from matplotlib import pyplot as plt
 import os, re
 from skimage import data, img_as_ubyte
 from skimage.filters import threshold_isodata, threshold_li, threshold_minimum, threshold_yen, threshold_triangle, threshold_otsu
+from display import *
+import math
 
+def findDartTip(before, after, pos):
+    diff = cv2.absdiff(before, after)
+    grayscale = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+    blurred = cv2.medianBlur(grayscale, 5)
 
-rootdir = 'C:/Users/alfre/Documents/cs310/images'
+    # Normalise
+    norm_img1 = cv2.normalize(blurred, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    norm_img1 = (255*norm_img1).astype(np.uint8)
+    # inverted = 255 - norm_img1
+
+    # # Thresholding operations
+    ret, thresh = cv2.threshold(norm_img1, 150, 255, cv2.THRESH_BINARY)
+    # ret3,otsuCV = cv2.threshold(norm_img1, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # adaptiveMeanThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 5)
+    # adaptiveGaussThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
+
+    
+
+    # triangle = threshold_triangle(norm_img1)
+    # yen = threshold_yen(norm_img1)
+    # li = threshold_li(norm_img1)
+    # min = threshold_minimum(norm_img1)
+    # otsu = threshold_otsu(norm_img1)
+    # isodata = threshold_isodata(norm_img1)
+    # ret, triangle = cv2.threshold(norm_img1, triangle, 255, cv2.THRESH_BINARY)
+    # ret, yen = cv2.threshold(norm_img1, yen, 255, cv2.THRESH_BINARY)
+    # ret, li = cv2.threshold(norm_img1, li, 255, cv2.THRESH_BINARY)
+    # ret, min = cv2.threshold(norm_img1, min, 255, cv2.THRESH_BINARY)
+    # ret, otsu = cv2.threshold(norm_img1, otsu, 255, cv2.THRESH_BINARY)
+    # ret, isodata = cv2.threshold(norm_img1, isodata, 255, cv2.THRESH_BINARY)
+
+    img = thresh
+
+    # all = np.zeros(img.shape)
+    # all = cv2.normalize(255 - adaptiveMeanThresh, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(yen, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(otsuCV, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(min, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(otsu, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(isodata, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(thresh, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(255 - adaptiveGaussThresh, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(triangle, all, 0, 25, cv2.NORM_MINMAX)\
+    #         + cv2.normalize(li, all, 0, 25, cv2.NORM_MINMAX)
+
+    # ret, all = cv2.threshold(all, 101, 255, cv2.THRESH_BINARY)
+    all = thresh
+
+    # Morphological operations to remove noise
+    # kernelOpen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,5))
+    # kernelClose = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,7))
+    # morphological = cv2.morphologyEx(all, cv2.MORPH_OPEN, kernelOpen)
+    # final = cv2.morphologyEx(all, cv2.MORPH_CLOSE, kernelClose)
+    final = thresh
+
+    # Find the lowest y point of the max size contour
+    contours, hierarchy = cv2.findContours(final, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key = cv2.contourArea)
+
+    blank = np.zeros(img.shape, dtype=np.uint8)
+
+    rows,cols = img.shape[:2]
+    [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+    lefty = int((-x*vy/vx) + y)
+    righty = int(((cols-x)*vy/vx)+y)
+    cv2.line(blank,(cols-1,righty),(0,lefty),(255,255,255),15)
+
+    blank = cv2.bitwise_and(blank, final)
+    y, x = np.nonzero(blank)
+    y = y[-1]
+    x = x[-1]
+    
+    # lowest = tuple(cnt[cnt[:, :, 1].argmax()][0])
+    # after = cv2.circle(after, (x,y), radius=2, color=(0, 0, 255), thickness=-1)
+
+    # # Plot the results
+    # ax1 = plt.subplot(1, 3, 2)
+    # ax1.imshow(cv2.cvtColor(after, cv2.COLOR_BGR2RGB))
+    # ax1.title.set_text('After')
+
+    # ax2 = plt.subplot(3, 5, 2, sharex=ax1, sharey=ax1)
+    # ax2.imshow(cv2.cvtColor(diff, cv2.COLOR_BGR2RGB))
+    # ax2.title.set_text('Difference')
+
+    ax3 = plt.subplot(1, 3, pos)
+    ax3.imshow(cv2.cvtColor(final, cv2.COLOR_BGR2RGB))
+    ax3.title.set_text('Final')
+
+    # ax4 = plt.subplot(3, 5, 4, sharex=ax1, sharey=ax1)
+    # ax4.imshow(cv2.cvtColor(blank, cv2.COLOR_BGR2RGB))
+    # ax4.title.set_text('Line')
+
+    # ax5 = plt.subplot(3, 5, 5, sharex=ax1, sharey=ax1)
+    # ax5.imshow(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
+    # ax5.title.set_text('Global')
+
+    # ax6 = plt.subplot(3, 5, 6, sharex=ax1, sharey=ax1)
+    # ax6.imshow(cv2.cvtColor(otsuCV, cv2.COLOR_BGR2RGB))
+    # ax6.title.set_text('OtsuCV')
+
+    # ax7 = plt.subplot(3, 5, 7, sharex=ax1, sharey=ax1)
+    # ax7.imshow(cv2.cvtColor(255 - adaptiveMeanThresh, cv2.COLOR_BGR2RGB))
+    # ax7.title.set_text('Mean')
+
+    # ax8 = plt.subplot(3, 5, 8, sharex=ax1, sharey=ax1)
+    # ax8.imshow(cv2.cvtColor(255 - adaptiveGaussThresh, cv2.COLOR_BGR2RGB))
+    # ax8.title.set_text('Gaussian')
+
+    # ax10 = plt.subplot(3, 5, 9, sharex=ax1, sharey=ax1)
+    # ax10.imshow(cv2.cvtColor(norm_img1, cv2.COLOR_BGR2RGB))
+    # ax10.title.set_text('Normalised')
+
+    # ax10 = plt.subplot(3, 5, 10, sharex=ax1, sharey=ax1)
+    # ax10.imshow(cv2.cvtColor(triangle, cv2.COLOR_BGR2RGB))
+    # ax10.title.set_text('Triangle')
+
+    # ax10 = plt.subplot(3, 5, 11, sharex=ax1, sharey=ax1)
+    # ax10.imshow(cv2.cvtColor(yen, cv2.COLOR_BGR2RGB))
+    # ax10.title.set_text('Yen')
+
+    # ax11 = plt.subplot(3, 5, 12, sharex=ax1, sharey=ax1)
+    # ax11.imshow(cv2.cvtColor(li, cv2.COLOR_BGR2RGB))
+    # ax11.title.set_text('Li')
+
+    # ax10 = plt.subplot(3, 5, 13, sharex=ax1, sharey=ax1)
+    # ax10.imshow(cv2.cvtColor(min, cv2.COLOR_BGR2RGB))
+    # ax10.title.set_text('Min')
+
+    # ax10 = plt.subplot(3, 5, 14, sharex=ax1, sharey=ax1)
+    # ax10.imshow(cv2.cvtColor(all, cv2.COLOR_BGR2RGB))
+    # ax10.title.set_text('All')
+
+    # ax11 = plt.subplot(3, 5, 15, sharex=ax1, sharey=ax1)
+    # ax11.imshow(cv2.cvtColor(isodata, cv2.COLOR_BGR2RGB))
+    # ax11.title.set_text('Isodata')
+
+    # ax1.set_xlim([x-60, x+60])
+    # ax1.set_ylim([y+50, max(0,y-300)])
+
+    # plt.show()
+
+    return(x, y)
+
+xFov = 65
+yFov = 65
+xOffset = 0
+yOffset = 0
+
+rootdir = 'C:/Users/alfre/Documents/cs310/images/labeled_images/'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if re.search("Before", file) is not None:
+        if re.search("x", file) is not None:
+            board = Dartboard(subdir)
+            board.drawPoint(0, -354)
+            board.drawPoint(-354, 0)
 
-            # Load the before and after images
-            before = cv2.imread(os.path.join(subdir, file))
-            after = cv2.imread(os.path.join(subdir, re.sub('Before', 'After', file)))
+            xBefore = cv2.imread('C:/Users/alfre/Documents/cs310/images/helper_images/empty/x_0.jpg')
+            xAfter = cv2.imread(os.path.join(subdir, file))
+            xX, xY = findDartTip(xBefore, xAfter, 2)
 
-            diff = cv2.absdiff(before, after)
-            grayscale = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
-            blurred = cv2.medianBlur(grayscale, 5)
+            xGrad = rotateGradient(angleToGradient(findAngle(xX, xAfter, xFov, 1558)))
 
-            # Normalise
-            norm_img1 = cv2.normalize(blurred, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-            norm_img1 = (255*norm_img1).astype(np.uint8)
-            inverted = 255 - norm_img1
+            fileY = re.sub('x', 'y', file)
+            yBefore = cv2.imread('C:/Users/alfre/Documents/cs310/images/helper_images/empty/y_0.jpg')
+            yAfter = cv2.imread(os.path.join(subdir, fileY))
+            yX, yY = findDartTip(yBefore, yAfter, 3)
 
-            # Thresholding operations
-            ret, thresh = cv2.threshold(norm_img1, 25, 255, cv2.THRESH_BINARY)
-            ret3,otsuCV = cv2.threshold(norm_img1, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            adaptiveMeanThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 5)
-            adaptiveGaussThresh = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
+            yGrad = angleToGradient(findAngle(yX, yAfter, yFov, 1448))
 
-            
+            board.drawLine(0, -354, xGrad)
+            board.drawLine(-354, 0, yGrad)
 
-            triangle = threshold_triangle(norm_img1)
-            yen = threshold_yen(norm_img1)
-            li = threshold_li(norm_img1)
-            min = threshold_minimum(norm_img1)
-            otsu = threshold_otsu(norm_img1)
-            isodata = threshold_isodata(norm_img1)
-            ret, triangle = cv2.threshold(norm_img1, triangle, 255, cv2.THRESH_BINARY)
-            ret, yen = cv2.threshold(norm_img1, yen, 255, cv2.THRESH_BINARY)
-            ret, li = cv2.threshold(norm_img1, li, 255, cv2.THRESH_BINARY)
-            ret, min = cv2.threshold(norm_img1, min, 255, cv2.THRESH_BINARY)
-            ret, otsu = cv2.threshold(norm_img1, otsu, 255, cv2.THRESH_BINARY)
-            ret, isodata = cv2.threshold(norm_img1, isodata, 255, cv2.THRESH_BINARY)
+            x, y = board.intersect(0, -354, xGrad, -354, 0, yGrad)
+            r, theta = board.cartesianToPolar(x, y)
+            board.score(r, theta)
+            print(subdir)
 
-            img = thresh
-
-            all = np.zeros(img.shape)
-            all = cv2.normalize(255 - adaptiveMeanThresh, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(yen, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(otsuCV, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(min, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(otsu, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(isodata, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(thresh, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(255 - adaptiveGaussThresh, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(triangle, all, 0, 25, cv2.NORM_MINMAX)\
-                    + cv2.normalize(li, all, 0, 25, cv2.NORM_MINMAX)
-
-            ret, all = cv2.threshold(all, 101, 255, cv2.THRESH_BINARY)
-
-            # Morphological operations to remove noise
-            kernelOpen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,5))
-            kernelClose = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,7))
-            morphological = cv2.morphologyEx(all, cv2.MORPH_OPEN, kernelOpen)
-            final = cv2.morphologyEx(all, cv2.MORPH_CLOSE, kernelClose)
-
-            # Find the lowest y point of the max size contour
-            contours, hierarchy = cv2.findContours(final, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cnt = max(contours, key = cv2.contourArea)
-
-            blank = np.zeros(img.shape, dtype=np.uint8)
-
-            rows,cols = img.shape[:2]
-            [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
-            lefty = int((-x*vy/vx) + y)
-            righty = int(((cols-x)*vy/vx)+y)
-            cv2.line(blank,(cols-1,righty),(0,lefty),(255,255,255),5)
-
-            blank = cv2.bitwise_and(blank, final)
-            y, x = np.nonzero(blank)
-            y = y[-1]
-            x = x[-1]
-            print(x, y)
-            # lowest = tuple(cnt[cnt[:, :, 1].argmax()][0])
-            after = cv2.circle(after, (x,y), radius=2, color=(0, 0, 255), thickness=-1)
-
-            # Plot the results
-            ax1 = plt.subplot(3, 5, 1)
-            ax1.imshow(cv2.cvtColor(after, cv2.COLOR_BGR2RGB))
-            ax1.title.set_text('After')
-
-            ax2 = plt.subplot(3, 5, 2, sharex=ax1, sharey=ax1)
-            ax2.imshow(cv2.cvtColor(diff, cv2.COLOR_BGR2RGB))
-            ax2.title.set_text('Difference')
-
-            ax3 = plt.subplot(3, 5, 3, sharex=ax1, sharey=ax1)
-            ax3.imshow(cv2.cvtColor(final, cv2.COLOR_BGR2RGB))
-            ax3.title.set_text('Final')
-
-            ax4 = plt.subplot(3, 5, 4, sharex=ax1, sharey=ax1)
-            ax4.imshow(cv2.cvtColor(blank, cv2.COLOR_BGR2RGB))
-            ax4.title.set_text('Line')
-
-            ax5 = plt.subplot(3, 5, 5, sharex=ax1, sharey=ax1)
-            ax5.imshow(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
-            ax5.title.set_text('Global')
-
-            ax6 = plt.subplot(3, 5, 6, sharex=ax1, sharey=ax1)
-            ax6.imshow(cv2.cvtColor(otsuCV, cv2.COLOR_BGR2RGB))
-            ax6.title.set_text('OtsuCV')
-
-            ax7 = plt.subplot(3, 5, 7, sharex=ax1, sharey=ax1)
-            ax7.imshow(cv2.cvtColor(255 - adaptiveMeanThresh, cv2.COLOR_BGR2RGB))
-            ax7.title.set_text('Mean')
-
-            ax8 = plt.subplot(3, 5, 8, sharex=ax1, sharey=ax1)
-            ax8.imshow(cv2.cvtColor(255 - adaptiveGaussThresh, cv2.COLOR_BGR2RGB))
-            ax8.title.set_text('Gaussian')
-
-            ax10 = plt.subplot(3, 5, 9, sharex=ax1, sharey=ax1)
-            ax10.imshow(cv2.cvtColor(norm_img1, cv2.COLOR_BGR2RGB))
-            ax10.title.set_text('Normalised')
-
-            ax10 = plt.subplot(3, 5, 10, sharex=ax1, sharey=ax1)
-            ax10.imshow(cv2.cvtColor(triangle, cv2.COLOR_BGR2RGB))
-            ax10.title.set_text('Triangle')
-
-            ax10 = plt.subplot(3, 5, 11, sharex=ax1, sharey=ax1)
-            ax10.imshow(cv2.cvtColor(yen, cv2.COLOR_BGR2RGB))
-            ax10.title.set_text('Yen')
-
-            ax11 = plt.subplot(3, 5, 12, sharex=ax1, sharey=ax1)
-            ax11.imshow(cv2.cvtColor(li, cv2.COLOR_BGR2RGB))
-            ax11.title.set_text('Li')
-
-            ax10 = plt.subplot(3, 5, 13, sharex=ax1, sharey=ax1)
-            ax10.imshow(cv2.cvtColor(min, cv2.COLOR_BGR2RGB))
-            ax10.title.set_text('Min')
-
-            ax10 = plt.subplot(3, 5, 14, sharex=ax1, sharey=ax1)
-            ax10.imshow(cv2.cvtColor(all, cv2.COLOR_BGR2RGB))
-            ax10.title.set_text('All')
-
-            ax11 = plt.subplot(3, 5, 15, sharex=ax1, sharey=ax1)
-            ax11.imshow(cv2.cvtColor(isodata, cv2.COLOR_BGR2RGB))
-            ax11.title.set_text('Isodata')
-
-            # ax1.set_xlim([x-40, x+40])
-            # ax1.set_ylim([y+40, y-40])
-
-            plt.show()
+            board.show()
+            board.close()
--- src/display.py
+++ src/display.py
@@ -2,62 +2,157 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Wedge
 import math
 
-fig, ax = plt.subplots()
+def findAngle(xPos, img, fov, center):
+    height, width, layers = img.shape
 
-# Set dimensions and force aspect ratio to be square
-ax.set_xlim([-350, 350])
-ax.set_ylim([350, -350])
-ax.set_box_aspect(1)
+    # fromCenter = xPos - (width/2)
+    fromCenter = xPos - center
+    ratio = fromCenter/width
 
-# Board parts
-foam = plt.Circle((0, 0), 337.5, color='gray')
-board = plt.Circle((0,0), 225.5, color='k')
-outerBull = plt.Circle((0,0), 16, color='g')
-innerBull = plt.Circle((0,0), 6.35, color='r')
+    angle = (ratio * fov)
 
-def drawSector(radius, color1, color2):
-    for i in range(0, 20):
-        theta1 = 9 + 18*(i-1)
-        theta2 = 9 + (18*i)
+    return angle
 
-        if i % 2 == 0:
-            sector = Wedge((0,0), radius, theta1, theta2, color=color1)
-        else:
-            sector = Wedge((0,0), radius, theta1, theta2, color=color2)
+def angleToGradient(angle):
+    if angle < 0:
+        slope = 1
+    if angle > 0:
+        slope = -1
 
-        ax.add_patch(sector)
+    angle = abs(angle)
 
-def drawLine():
-    for i in range(0, 20):
-        x1 = math.cos((9/180)*math.pi + 2*math.pi/20*i)*16
-        y1 = math.sin((9/180)*math.pi + 2*math.pi/20*i)*16
-
-        x2 = math.cos((9/180)*math.pi + 2*math.pi/20*i)*170
-        y2 = math.sin((9/180)*math.pi + 2*math.pi/20*i)*170
+    gradient = math.sin(math.radians(angle)) / math.cos(math.radians(angle))
+    gradient *= slope
     
-        x1, y1 = [x1, x2], [y1, y2]
-        ax.plot(x1, y1, color='gray', linewidth = '1')
+    return gradient
 
-ax.add_patch(foam)
-ax.add_patch(board)
+def rotateGradient(gradient):
+    return -1 * (1/gradient)
 
-drawSector(170, 'g', 'r')
-drawSector(162, 'k', 'k')
-drawSector(107, 'g', 'r')
-drawSector(99, 'k', 'k')
+class Dartboard():
+    def __init__(self, title):
+        # self.fig, self.ax = plt.subplots(1, 2, 1)
+        self.ax = plt.subplot(1, 3, 1)
 
-drawLine()
+        # Set dimensions and force aspect ratio to be square
+        self.ax.set_xlim([-350, 350])
+        self.ax.set_ylim([350, -350])
+        self.ax.set_box_aspect(1)
+        plt.gca().set_aspect('equal')
 
-ax.add_patch(outerBull)
-ax.add_patch(innerBull)
+        # self.fig.suptitle(title)
+        self.ax.title.set_text(title)
 
-ax.plot(0, -335, color='m', marker='+')
-ax.plot(-335, 0, color='m', marker='+')
-ax.plot(335, 0, color='m', marker='+')
+        self.drawBoard()
 
-ax.axline((0, -335), (-10, -45), linewidth=1, color='c')
-ax.axline((-335, 0), (-10, -45), linewidth=1, color='c')
-ax.axline((335, 0), (-10, -45), linewidth=1, color='c')
+        self.ax.add_patch(self.foam)
+        self.ax.add_patch(self.board)
 
-plt.gca().set_aspect('equal')
-plt.show()
+        self.drawSector(170, 'g', 'r')
+        self.drawSector(162, 'w', 'k')
+        self.drawSector(107, 'g', 'r')
+        self.drawSector(99, 'w', 'k')
+
+        self.drawRadius()
+
+        self.ax.add_patch(self.outerBull)
+        self.ax.add_patch(self.innerBull)
+
+    def drawBoard(self):
+        # Board parts
+        self.foam = plt.Circle((0, 0), 337.5, color='gray')
+        self.board = plt.Circle((0,0), 225.5, color='k')
+        self.outerBull = plt.Circle((0,0), 16, color='g')
+        self.innerBull = plt.Circle((0,0), 6.35, color='r')
+
+    def drawSector(self, radius, color1, color2):
+        for i in range(0, 20):
+            theta1 = 9 + 18*(i-1)
+            theta2 = 9 + (18*i)
+
+            if i % 2 == 0:
+                sector = Wedge((0,0), radius, theta1, theta2, color=color1)
+            else:
+                sector = Wedge((0,0), radius, theta1, theta2, color=color2)
+
+            self.ax.add_patch(sector)
+
+    def drawRadius(self):
+        for i in range(0, 20):
+            x1 = math.cos((9/180)*math.pi + 2*math.pi/20*i)*16
+            y1 = math.sin((9/180)*math.pi + 2*math.pi/20*i)*16
+
+            x2 = math.cos((9/180)*math.pi + 2*math.pi/20*i)*170
+            y2 = math.sin((9/180)*math.pi + 2*math.pi/20*i)*170
+        
+            x1, y1 = [x1, x2], [y1, y2]
+            self.ax.plot(x1, y1, color='gray', linewidth = '1')
+
+    def show(self):
+        plt.show()
+    
+    def close(self):
+        plt.close()
+
+    def drawPoint(self, xPos, yPos):
+        self.ax.plot(xPos, yPos, color='magenta', marker='x')
+
+    def drawLine(self, xPos, yPos, gradient):
+        self.ax.axline((xPos, yPos), (xPos+10, yPos-10*gradient), linewidth=1, color='cyan')
+
+    def intersect(self, xPos1, yPos1, gradient1, xPos2, yPos2, gradient2):
+        yPos1 = -yPos1
+        yPos2 = -yPos2
+
+        c1 = yPos1 - (xPos1*gradient1)
+        c2 = yPos2 - (xPos2*gradient2)
+
+        x = (c2 - c1) / (gradient1 - gradient2)
+        y = gradient1*x + c1
+
+        self.drawPoint(x, -y)
+
+        return (x, y)
+
+    def cartesianToPolar(self, x, y):
+        r = math.sqrt(x**2 + y**2)
+
+        if x > 0:
+            theta = math.atan(y/x)
+        else:
+            theta = math.atan(y/x) + math.pi
+        
+        return (r, theta)
+    
+    def score(self, r, theta):
+        sectors = [6,13,4,18,1,20,5,12,9,14,11,8,16,7,19,3,17,2,15,10]
+        sector = math.floor(((theta)/(2*math.pi))*20 + 1/2)
+        value = str(sectors[sector])
+
+        if 170 < r:
+            pass
+        elif 162 < r:
+            print('Double ' + value)
+        elif 107 < r:
+            print('Single ' + value)
+        elif 99 < r:
+            print('Tripple ' + value)
+        elif 16 < r:
+            print('Single ' + value)
+        elif 6.35 < r:
+            print('Outer Bull')
+        else:
+            print('Inner Bull')
+
+# topRatio = 0.10304878048780487
+# sideRatio = 0.46463414634146344
+
+# fov = 175
+
+# topAngle = math.radians(fov*topRatio)
+# sideAngle = math.radians(fov*sideRatio)
+
+# fromTop(10, -310 + 10*math.sin(topAngle)/math.cos(topAngle))
+# fromSide(-310, -10*math.sin(sideAngle)/math.cos(sideAngle))
+
+# plt.show()
